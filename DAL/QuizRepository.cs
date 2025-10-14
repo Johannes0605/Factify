5d36f75cb0ec58ff692ec502afd03d3c31b860cb
Sunniva-Sorensen <sunnsor@gmail.com>
2025-10-14 14:43:57 +0200

    Update quiz editor

--- DAL/QuizRepository.cs
+++ DAL/QuizRepository.cs
@@ -36,8 +36,84 @@ namespace QuizApp.DAL
 
         public async Task UpdateQuizAsync(Quiz quiz)
         {
-            _context.Quizzes.Update(quiz);
-            await _context.SaveChangesAsync();
+            var existingQuiz = await _context.Quizzes
+                .Include(q => q.Questions)
+                    .ThenInclude(qs => qs.Options)
+                .FirstOrDefaultAsync(q => q.QuizId == quiz.QuizId);
+
+            if (existingQuiz != null)
+            {
+                // Update quiz properties
+                existingQuiz.Title = quiz.Title;
+                existingQuiz.Description = quiz.Description;
+
+                // Remove questions that are no longer in the list
+                var questionsToRemove = existingQuiz.Questions
+                    .Where(eq => !quiz.Questions.Any(nq => nq.QuestionId == eq.QuestionId))
+                    .ToList();
+                
+                foreach (var question in questionsToRemove)
+                {
+                    _context.Questions.Remove(question);
+                }
+
+                // Update or add questions
+                foreach (var question in quiz.Questions)
+                {
+                    if (question.QuestionId == 0)
+                    {
+                        // New question
+                        question.QuizId = quiz.QuizId;
+                        existingQuiz.Questions.Add(question);
+                    }
+                    else
+                    {
+                        // Update existing question
+                        var existingQuestion = existingQuiz.Questions
+                            .FirstOrDefault(q => q.QuestionId == question.QuestionId);
+                        
+                        if (existingQuestion != null)
+                        {
+                            existingQuestion.QuestionText = question.QuestionText;
+
+                            // Remove options that are no longer in the list
+                            var optionsToRemove = existingQuestion.Options
+                                .Where(eo => !question.Options.Any(no => no.OptionsId == eo.OptionsId))
+                                .ToList();
+                            
+                            foreach (var option in optionsToRemove)
+                            {
+                                _context.Options.Remove(option);
+                            }
+
+                            // Update or add options
+                            foreach (var option in question.Options)
+                            {
+                                if (option.OptionsId == 0)
+                                {
+                                    // New option
+                                    option.QuestionId = question.QuestionId;
+                                    existingQuestion.Options.Add(option);
+                                }
+                                else
+                                {
+                                    // Update existing option
+                                    var existingOption = existingQuestion.Options
+                                        .FirstOrDefault(o => o.OptionsId == option.OptionsId);
+                                    
+                                    if (existingOption != null)
+                                    {
+                                        existingOption.Text = option.Text;
+                                        existingOption.IsCorrect = option.IsCorrect;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                await _context.SaveChangesAsync();
+            }
         }
 
         public async Task DeleteQuizAsync(int id)
